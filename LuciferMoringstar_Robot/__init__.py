e03830ebe56f108f3b1a1532253faf1c7df6f044
gangstar1010 <88017311+gangstar1010@users.noreply.github.com>
2021-09-30 20:42:43 +0530

    Update __init__.py

--- LuciferMoringstar_Robot/__init__.py
+++ LuciferMoringstar_Robot/__init__.py
@@ -25,19 +25,19 @@ Bot Owner Only
 /logger  Get Logs
 /delete Delete File From Database
 
-[➡️How To Add Old Files For database](https://youtu.be/mtu5hgW-l_o)
+[➡️Join Our All Movies ](https://t.me/SH_Telugu_All_Movies)
 
-@Mo_Tech_YT
+@SH_Telugu_All_Movies
 """
 
 ABOUT = """
 ➥ Name :  Auto Filter With IMDB
     
-➥ Creator : [Muhammed RK](t.me/Mrk_YT)
+➥ Creator : [Sandesh](http://t.me/SHProSearchbot)
 
 ➥ Language : Python3
 
 ➥ Library : Pyrogram Asyncio 1.13.0
 
-➥ Source Code : [Click Me](https://github.com/PR0FESS0R-99/LuciferMoringstar_Robot)
+➥ Support Group : [Click Me](https://t.me/KAANNADANEWMOVIEREQUESTINGGROUP)
 """
